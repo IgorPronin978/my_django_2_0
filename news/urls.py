ff545b85c54766384dcdb899c3f0de08c8095568
Igor Pronin <igp78@mail.ru>
2025-02-03 19:16:43 +0100

    исправлен upper_words

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # будет иметь префикс в urlах /news/
 urlpatterns = [
-    path('', views.get_all_news),
+    path('catalog/', views.get_all_news),
     path('catalog/', views.catalog, name='catalog'),
     path('catalog/<int:article_id>/', views.get_detail_article_by_id, name='detail_article_by_id'),
     path('catalog/<slug:slug>/', views.category_by_name),
