4bac97b002c5b29db1cd62b42fd1d456bbb052f7
Igor Pronin <igp78@mail.ru>
2025-02-10 21:07:01 +0100

    урок 9.1

--- itg/settings.py
+++ itg/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'itg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_django_2_0',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
