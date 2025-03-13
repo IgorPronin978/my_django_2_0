2fcc3591507e9ed85f6beb0f67933d9966c3236f
Igor Pronin <igp78@mail.ru>
2025-03-13 22:15:08 +0100

    загрузка новостей из файла JSON

--- news/templatetags/customtags.py
+++ news/templatetags/customtags.py
@@ -11,3 +11,7 @@ def has_liked(article, ip_address):
 @register.filter(name='has_favorited')
 def has_favorited(article, ip_address):
     return Favorite.objects.filter(article=article, ip_address=ip_address).exists()
+
+@register.filter
+def add(value, arg):
+    return int(value) + int(arg)
